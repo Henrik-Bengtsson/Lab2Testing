a365ff5c89117c5ae987f158465c52c90cb6829a
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-01-27 14:26:03 +0100

    Add test for Employee1 isPaid set to false when thrown exception

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -11,11 +11,12 @@ class EmployeesTest {
     BankService bankService = Mockito.mock(BankService.class);
     EmployeeRepositoryImpl employeeRepository = new EmployeeRepositoryImpl();
     Employees employees = new Employees(employeeRepository, bankService);
+    Employee employee1, employee2;
 
     @BeforeEach
     void setUp() {
-        Employee employee1 = new Employee("1", 50000);
-        Employee employee2 = new Employee("2", 70000);
+        employee1 = new Employee("1", 50000);
+        employee2 = new Employee("2", 70000);
         employeeRepository.save(employee1);
         employeeRepository.save(employee2);
     }
@@ -29,5 +30,12 @@ class EmployeesTest {
     void testNumberDecreasingWhenThrowException() {
         Mockito.doThrow(RuntimeException.class).when(bankService).pay("2", 70000);
         assertThat(employees.payEmployees()).isEqualTo(1);
+    }
+
+    @Test
+    void testEmployee1IsPaidSetToFalseWhenThrowException() {
+        Mockito.doThrow(RuntimeException.class).when(bankService).pay("1", 50000);
+        employees.payEmployees();
+        assertThat(employeeRepository.findAll().get(0).isPaid()).isFalse();
     }
 }
