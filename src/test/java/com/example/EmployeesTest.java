ebad34f00ebbdbfbc5f9844a75b2af674ca83a4d
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-01-27 14:13:06 +0100

    Add test for decreasing number when throw exception

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -7,18 +8,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class EmployeesTest {
 
-    EmployeeRepositoryImpl employeeRepository = new EmployeeRepositoryImpl();
     BankService bankService = Mockito.mock(BankService.class);
-    
+    EmployeeRepositoryImpl employeeRepository = new EmployeeRepositoryImpl();
     Employees employees = new Employees(employeeRepository, bankService);
 
-    @Test
-    void TestNumberOfPaidEmployeesShouldReturnTwo() {
+    @BeforeEach
+    void setUp() {
         Employee employee1 = new Employee("1", 50000);
         Employee employee2 = new Employee("2", 70000);
         employeeRepository.save(employee1);
         employeeRepository.save(employee2);
+    }
 
+    @Test
+    void TestNumberOfPaidEmployeesShouldReturnTwo() {
         assertThat(employees.payEmployees()).isEqualTo(2);
+    }
+
+    @Test
+    void testNumberDecreasingWhenThrowException() {
+        Mockito.doThrow(RuntimeException.class).when(bankService).pay("2", 70000);
+        assertThat(employees.payEmployees()).isEqualTo(1);
     }
 }
