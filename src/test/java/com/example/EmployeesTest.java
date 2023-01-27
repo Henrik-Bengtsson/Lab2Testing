32228e089089d8c98de236660386b5998f989eb7
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-01-27 13:18:52 +0100

    Add test for number paid employees

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,5 +1,24 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class EmployeesTest {
 
+    EmployeeRepositoryImpl employeeRepository = new EmployeeRepositoryImpl();
+    BankService bankService = Mockito.mock(BankService.class);
+    
+    Employees employees = new Employees(employeeRepository, bankService);
+
+    @Test
+    void TestNumberOfPaidEmployeesShouldReturnTwo() {
+        Employee employee1 = new Employee("1", 50000);
+        Employee employee2 = new Employee("2", 70000);
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+
+        assertThat(employees.payEmployees()).isEqualTo(2);
+    }
 }
