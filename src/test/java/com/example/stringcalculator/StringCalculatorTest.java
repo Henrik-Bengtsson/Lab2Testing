47efc4a15b8764ff460e38460befa400d0a59dec
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-01-30 13:07:02 +0100

    Clean-up tests

--- src/test/java/com/example/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/example/stringcalculator/StringCalculatorTest.java
@@ -2,49 +2,50 @@ package com.example.stringcalculator;
 
 import org.junit.jupiter.api.Test;
 
+import static com.example.stringcalculator.StringCalculator.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
 
     @Test
     void returnZeroForEmptyString() {
-        int result = StringCalculator.add("");
+        int result = add("");
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     void returnOneWithOneAsInput() {
-        int result = StringCalculator.add("1");
+        int result = add("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     void returnThreeWithOneAndTwoAsInput() {
-        int result = StringCalculator.add("1,2");
+        int result = add("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     void returnNineWithFourAndFiveAsInput() {
-        int result = StringCalculator.add("4,5");
+        int result = add("4,5");
         assertThat(result).isEqualTo(9);
     }
 
     @Test
     void inputFourNumbersAndReturnSum18() {
-        int result = StringCalculator.add("3,4,5,6");
+        int result = add("3,4,5,6");
         assertThat(result).isEqualTo(18);
     }
 
     @Test
     void shouldReturn6WhenInputContainsNewLine() {
-        int result = StringCalculator.add("1\n2,3");
+        int result = add("1\n2,3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     void shouldReturn5WithDifferentDelimiters() {
-        int result = StringCalculator.add("//;\\n2;3");
+        int result = add("//;\\n2;3");
         assertThat(result).isEqualTo(5);
     }
 }
